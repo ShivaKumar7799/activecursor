4c5bba6918336bbffcdf0c43438f700689750b5e
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2024-08-10 20:31:48 +0530

    active input field cursor on rerendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
 
 function App() {
+  const [values, setValues] = useState(['', '', '']);
+  const [selectionRanges, setSelectionRanges] = useState([
+    { beforeStart: 0, beforeEnd: 0 },
+    { beforeStart: 0, beforeEnd: 0 },
+    { beforeStart: 0, beforeEnd: 0 },
+  ]);
+
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    inputRefs.current = inputRefs.current.slice(0, values.length);
+    inputRefs.current = values.map(
+      (_, i) => inputRefs.current[i] || React.createRef()
+    );
+  }, [values]);
+
+  useLayoutEffect(() => {
+    inputRefs.current.forEach((ref, index) => {
+      if (ref.current) {
+        ref.current.setSelectionRange(
+          selectionRanges[index].beforeStart,
+          selectionRanges[index].beforeEnd
+        );
+      }
+    });
+  }, [values, selectionRanges]);
+
+  const handleInputChange = (index, e) => {
+    const newValues = [...values];
+    newValues[index] = e.target.value.toUpperCase();
+    setValues(newValues);
+
+    const newSelectionRanges = [...selectionRanges];
+    newSelectionRanges[index] = {
+      beforeStart: e.target.selectionStart,
+      beforeEnd: e.target.selectionEnd,
+    };
+    setSelectionRanges(newSelectionRanges);
+  };
+
+  const addInputField = () => {
+    setValues((prev) => [...prev, '']);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputFields
+        values={values}
+        inputRefs={inputRefs}
+        onInputChange={handleInputChange}
+      />
+      <button onClick={addInputField}>+</button>
     </div>
   );
 }
 
 export default App;
+
+function InputFields({ values, inputRefs, onInputChange }) {
+  return (
+    <div>
+      {values.map((value, index) => (
+        <input
+          key={index}
+          ref={inputRefs.current[index]}
+          value={value}
+          onChange={(e) => onInputChange(index, e)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export { InputFields };
